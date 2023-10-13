1df9def1cef114d600f5fac1be4fa7fe083e01b4
Bharath Vedartham <vedabharath12345@gmail.com>
2023-10-13 17:14:29 +0530

    log

--- builder/builder.go
+++ builder/builder.go
@@ -345,6 +345,8 @@ func (b *Builder) OnPayloadAttribute(attrs *types.BuilderPayloadAttributes) erro
 	if err != nil {
 		return fmt.Errorf("could not get validator while submitting block for slot %d - %w", attrs.Slot, err)
 	}
+	log.Info("DEBUG: Builder public key is ", "pk", b.builderPublicKey.String())
+	log.Info("DEBUG: Builder secret key is ", "pk", b.builderSecretKey.String())
 	tobReservations, err := b.relay.GetTobGasReservations()
 	if err != nil {
 		return fmt.Errorf("could not get tob reservations - %w", err)

15edb3fc033fe615fc1899496231a34c3e9aec86
Bharath Vedartham <vedabharath12345@gmail.com>
2023-10-13 20:20:03 +0530

    remove logs

--- builder/builder.go
+++ builder/builder.go
@@ -341,9 +341,6 @@ func (b *Builder) OnPayloadAttribute(attrs *types.BuilderPayloadAttributes) erro
 		return nil
 	}
 
-	log.Info("DEBUG: Builder public key is ", "pk", b.builderPublicKey.String())
-	log.Info("DEBUG: Builder secret key is ", "pk", b.builderSecretKey.)
-
 	vd, err := b.relay.GetValidatorForSlot(attrs.Slot)
 	if err != nil {
 		return fmt.Errorf("could not get validator while submitting block for slot %d - %w", attrs.Slot, err)
